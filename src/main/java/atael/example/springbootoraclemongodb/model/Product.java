4ed0f338ab563a9bbe2672ff7c24a61dec51baca
Andy Tael <andy.tael@yahoo.com>
2025-02-13 13:49:17 -0600

    Doc update

--- src/main/java/atael/example/springbootoraclemongodb/model/Product.java
+++ src/main/java/atael/example/springbootoraclemongodb/model/Product.java
@@ -7,8 +7,12 @@ import org.springframework.data.mongodb.core.mapping.Document;
 @Document(collection = "products")
 @Data
 public class Product {
+
+  // In MongoDB, each document stored in a standard collection requires a unique _id field that acts as a primary key. 
+  // If an inserted document omits the _id field, the MongoDB driver automatically generates an ObjectId for the _id field.
+  
   @Id
-  private String productId;
+  private String productId; // Mapped to _id in the document
   private String name;
   private String category;
   private Double price;
